b84a40f198c6582b9356e38061dac4cede1b7a05
aggresivveqq <china02062004@mail.ru>
2025-08-25 22:30:11 +0500

    qwe

---
+++ мусор/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace мусор.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
